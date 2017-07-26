7204c8b68faff4a6ac0ff3591e72a6b538e08035
Your Name <you@example.com>
2017-07-26 10:01:59 +0800

    secess then end for a while

---
+++ py_data/views2.py
@@ -0,0 +1,267 @@
+from django.shortcuts import render 
+
+
+def wxb1_gk_index(request):
+    return render(request, "appo/wxb1_gk_index.html")
+
+
+def wxb1_gk_page_class_alert1(request):
+    return render(request, "appo/wxb1_gk_page_class_alert1.html")
+
+
+def wxb1_gk_page_class_alert2(request):
+    return render(request, "appo/wxb1_gk_page_class_alert2.html")
+
+
+def wxb1_gk_page_class_cookie(request):
+    return render(request, "appo/wxb1_gk_page_class_cookie.html")
+
+
+def wxb1_gk_page_class_force1(request):
+    return render(request, "appo/wxb1_gk_page_class_force1.html")
+
+
+def wxb1_gk_page_class_force2(request):
+    return render(request, "appo/wxb1_gk_page_class_force2.html")
+
+
+def wxb1_gk_page_class_map(request):
+    return render(request, "appo/wxb1_gk_page_class_map.html")
+
+
+def wxb1_gk_page_class_mapTarget(request):
+    return render(request, "appo/wxb1_gk_page_class_mapTarget.html")
+
+
+def wxb1_gk_page_class_new51(request):
+    return render(request, "appo/wxb1_gk_page_class_new51.html")
+
+
+def wxb1_gk_page_class_new61(request):
+    return render(request, "appo/wxb1_gk_page_class_new61.html")
+
+
+def wxb1_gk_page_class_new71(request):
+    return render(request, "appo/wxb1_gk_page_class_new71.html")
+
+
+def wxb1_gk_page_class_oa_oa(request):
+    return render(request, "appo/wxb1_gk_page_class_oa_oa.html")
+
+
+def wxb1_gk_page_class_port1(request):
+    return render(request, "appo/wxb1_gk_page_class_port1.html")
+
+
+def wxb1_gk_page_class_port2(request):
+    return render(request, "appo/wxb1_gk_page_class_port2.html")
+
+
+def wxb1_gk_page_class_prepared1(request):
+    return render(request, "appo/wxb1_gk_page_class_prepared1.html")
+
+
+def wxb1_gk_page_class_prepared2(request):
+    return render(request, "appo/wxb1_gk_page_class_prepared2.html")
+
+
+def wxb1_gk_page_class_prepared3(request):
+    return render(request, "appo/wxb1_gk_page_class_prepared3.html")
+
+
+def wxb1_gk_page_class_prepared4(request):
+    return render(request, "appo/wxb1_gk_page_class_prepared4.html")
+
+
+def wxb1_gk_page_class_topoFinally(request):
+    return render(request, "appo/wxb1_gk_page_class_topoFinally.html")
+
+
+def wxb1_gk_page_class_vnc_ubuntu(request):
+    return render(request, "appo/wxb1_gk_page_class_vnc_ubuntu.html")
+
+
+def wxb1_gk_page_class_vnc_win(request):
+    return render(request, "appo/wxb1_gk_page_class_vnc_win.html")
+
+
+def wxb1_index(request):
+    return render(request, "appo/wxb1_index.html")
+
+
+def wxb1_index2(request):
+    return render(request, "appo/wxb1_index2.html")
+
+
+def wxb1_lesuo_index(request):
+    return render(request, "appo/wxb1_lesuo_index.html")
+
+
+def wxb1_lesuo_page_class_alert1(request):
+    return render(request, "appo/wxb1_lesuo_page_class_alert1.html")
+
+
+def wxb1_lesuo_page_class_alert2(request):
+    return render(request, "appo/wxb1_lesuo_page_class_alert2.html")
+
+
+def wxb1_lesuo_page_class_cookie(request):
+    return render(request, "appo/wxb1_lesuo_page_class_cookie.html")
+
+
+def wxb1_lesuo_page_class_force1(request):
+    return render(request, "appo/wxb1_lesuo_page_class_force1.html")
+
+
+def wxb1_lesuo_page_class_force2(request):
+    return render(request, "appo/wxb1_lesuo_page_class_force2.html")
+
+
+def wxb1_lesuo_page_class_map(request):
+    return render(request, "appo/wxb1_lesuo_page_class_map.html")
+
+
+def wxb1_lesuo_page_class_mapTarget(request):
+    return render(request, "appo/wxb1_lesuo_page_class_mapTarget.html")
+
+
+def wxb1_lesuo_page_class_new51(request):
+    return render(request, "appo/wxb1_lesuo_page_class_new51.html")
+
+
+def wxb1_lesuo_page_class_new61(request):
+    return render(request, "appo/wxb1_lesuo_page_class_new61.html")
+
+
+def wxb1_lesuo_page_class_new71(request):
+    return render(request, "appo/wxb1_lesuo_page_class_new71.html")
+
+
+def wxb1_lesuo_page_class_oa_oa(request):
+    return render(request, "appo/wxb1_lesuo_page_class_oa_oa.html")
+
+
+def wxb1_lesuo_page_class_port1(request):
+    return render(request, "appo/wxb1_lesuo_page_class_port1.html")
+
+
+def wxb1_lesuo_page_class_port2(request):
+    return render(request, "appo/wxb1_lesuo_page_class_port2.html")
+
+
+def wxb1_lesuo_page_class_prepared1(request):
+    return render(request, "appo/wxb1_lesuo_page_class_prepared1.html")
+
+
+def wxb1_lesuo_page_class_prepared2(request):
+    return render(request, "appo/wxb1_lesuo_page_class_prepared2.html")
+
+
+def wxb1_lesuo_page_class_prepared3(request):
+    return render(request, "appo/wxb1_lesuo_page_class_prepared3.html")
+
+
+def wxb1_lesuo_page_class_prepared4(request):
+    return render(request, "appo/wxb1_lesuo_page_class_prepared4.html")
+
+
+def wxb1_lesuo_page_class_topoFinally(request):
+    return render(request, "appo/wxb1_lesuo_page_class_topoFinally.html")
+
+
+def wxb1_lesuo_page_class_vnc_ubuntu(request):
+    return render(request, "appo/wxb1_lesuo_page_class_vnc_ubuntu.html")
+
+
+def wxb1_lesuo_page_class_vnc_win(request):
+    return render(request, "appo/wxb1_lesuo_page_class_vnc_win.html")
+
+
+def wxb1_web_index(request):
+    return render(request, "appo/wxb1_web_index.html")
+
+
+def wxb1_web_page_class_alert1(request):
+    return render(request, "appo/wxb1_web_page_class_alert1.html")
+
+
+def wxb1_web_page_class_alert2(request):
+    return render(request, "appo/wxb1_web_page_class_alert2.html")
+
+
+def wxb1_web_page_class_cookie(request):
+    return render(request, "appo/wxb1_web_page_class_cookie.html")
+
+
+def wxb1_web_page_class_force(request):
+    return render(request, "appo/wxb1_web_page_class_force.html")
+
+
+def wxb1_web_page_class_force1(request):
+    return render(request, "appo/wxb1_web_page_class_force1.html")
+
+
+def wxb1_web_page_class_force2(request):
+    return render(request, "appo/wxb1_web_page_class_force2.html")
+
+
+def wxb1_web_page_class_map(request):
+    return render(request, "appo/wxb1_web_page_class_map.html")
+
+
+def wxb1_web_page_class_mapTarget(request):
+    return render(request, "appo/wxb1_web_page_class_mapTarget.html")
+
+
+def wxb1_web_page_class_new51(request):
+    return render(request, "appo/wxb1_web_page_class_new51.html")
+
+
+def wxb1_web_page_class_new61(request):
+    return render(request, "appo/wxb1_web_page_class_new61.html")
+
+
+def wxb1_web_page_class_new71(request):
+    return render(request, "appo/wxb1_web_page_class_new71.html")
+
+
+def wxb1_web_page_class_oa_oa(request):
+    return render(request, "appo/wxb1_web_page_class_oa_oa.html")
+
+
+def wxb1_web_page_class_port1(request):
+    return render(request, "appo/wxb1_web_page_class_port1.html")
+
+
+def wxb1_web_page_class_port2(request):
+    return render(request, "appo/wxb1_web_page_class_port2.html")
+
+
+def wxb1_web_page_class_prepared1(request):
+    return render(request, "appo/wxb1_web_page_class_prepared1.html")
+
+
+def wxb1_web_page_class_prepared2(request):
+    return render(request, "appo/wxb1_web_page_class_prepared2.html")
+
+
+def wxb1_web_page_class_prepared3(request):
+    return render(request, "appo/wxb1_web_page_class_prepared3.html")
+
+
+def wxb1_web_page_class_prepared4(request):
+    return render(request, "appo/wxb1_web_page_class_prepared4.html")
+
+
+def wxb1_web_page_class_topoFinally(request):
+    return render(request, "appo/wxb1_web_page_class_topoFinally.html")
+
+
+def wxb1_web_page_class_vnc_ubuntu(request):
+    return render(request, "appo/wxb1_web_page_class_vnc_ubuntu.html")
+
+
+def wxb1_web_page_class_vnc_win(request):
+    return render(request, "appo/wxb1_web_page_class_vnc_win.html")
+
+
